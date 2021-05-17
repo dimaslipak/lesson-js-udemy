b3e696f7bdf17c0edd5356bf04938ce5146f3d57
Dzmitry Dziahileu <dimaslipak@gmail.com>
2021-05-17 17:03:20 +0100

    00_commit

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+// alert('Are you here?');
+
+// const question = +prompt('How old are you?', '');
+// alert(typeof(question));
+
+// const answers = [];
+
+// for (let i = 0; i < 4; i++) {
+//     answers[i] = prompt('Введите число', '');
+// }
+// console.log(answers);
+
+// const answers = confirm('Are you here?');
+// console.log(answers);
+
+
+const name1 = 'Dima';
+
+alert(`Hello, ${name1}`); 
